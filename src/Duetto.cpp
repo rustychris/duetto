fedae94f9f182f7ec4f5ef54696db1a35b752848
Rusty Holleman <rustychris@gmail.com>
2021-09-29 18:09:48 -0500

    Light working

--- src/Duetto.cpp
+++ src/Duetto.cpp
@@ -3,18 +3,12 @@
 #define NNOTES 10
 #define NSTEPS 8
 
+#define NCOLORS 3
+
 struct Duetto : Module {
   enum ParamIds {
-    NOTE00_PARAM,
-    NOTE01_PARAM,
-    NOTE02_PARAM,
-    NOTE03_PARAM,
-    NOTE04_PARAM,
-    NOTE05_PARAM,
-    NOTE06_PARAM,
-    NOTE07_PARAM,
-    NOTE08_PARAM,
-    NOTE09_PARAM,
+    ENUMS(NOTE_PARAM,NNOTES),
+    ENUMS(STEP_PARAM, NSTEPS),
     NUM_PARAMS
   };
   enum InputIds {
@@ -25,57 +19,73 @@ struct Duetto : Module {
     NUM_OUTPUTS
   };
   enum LightIds {
-    ENUMS(STEP_LIGHT, 8),
-    // LIGHT03_LIGHT,
-    // LIGHT04_LIGHT,
-    // LIGHT02_LIGHT,
-    // LIGHT05_LIGHT,
-    // LIGHT01_LIGHT,
-    // LIGHT06_LIGHT,
-    // LIGHT00_LIGHT,
-    // LIGHT07_LIGHT,
+    ENUMS(STEP_LIGHT, NCOLORS*NSTEPS),
     NUM_LIGHTS
   };
 
-  float noteValues[NNOTES]={-12/12.,-9/12.,-7/12., -5/12.,-2./12,
-                             0, 3/12., 5/12., 7/12., 10/12.};
+  // Minor pentatonic. and an Off note
+  float noteValues[NNOTES+1]={-12/12.,-9/12.,-7/12., -5/12.,-2./12,
+                            0, 3/12., 5/12., 7/12., 10/12.,
+                            -10};
 
+  // why is this necessary?
   dsp::BooleanTrigger noteTriggers[NNOTES];
+  dsp::BooleanTrigger stepTriggers[NSTEPS];
   
   float phase; // signal output
   float metroPhase; // metronome/clock
   float pitches[NSTEPS];
   int step;
-  ParamIds NoteParams[NNOTES]={NOTE00_PARAM,
-                               NOTE01_PARAM,
-                               NOTE02_PARAM,
-                               NOTE03_PARAM,
-                               NOTE04_PARAM,
-                               NOTE05_PARAM,
-                               NOTE06_PARAM,
-                               NOTE07_PARAM,
-                               NOTE08_PARAM,
-                               NOTE09_PARAM};
 
+  float stepColor[NSTEPS][NCOLORS];
+  float noteColor[NNOTES+1][NCOLORS]={ {1.0, 0.0, 0.0},
+                                       {0.5, 0.5, 0.0},
+                                       {0.0, 1.0, 0.0},
+                                       {0.0, 0.5, 0.5},
+                                       {0.0, 0.0, 1.0},
+                                       
+                                       {1.0, 0.0, 0.0},
+                                       {0.5, 0.5, 0.0},
+                                       {0.0, 1.0, 0.0},
+                                       {0.0, 0.5, 0.5},
+                                       {0.0, 0.0, 1.0},
+
+                                       {1.0, 1.0, 1.0}, // no note
+  };
+  
   Duetto() {
     config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-    configParam(NOTE00_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(NOTE01_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(NOTE02_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(NOTE03_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(NOTE04_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(NOTE05_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(NOTE06_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(NOTE07_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(NOTE08_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(NOTE09_PARAM, 0.f, 1.f, 0.f, "");
+    for(int n=0;n<NNOTES;n++)
+      configParam(NOTE_PARAM+n, 0.f, 1.f, 0.f, "");
 
     for(int i=0;i<NSTEPS;i++) {
-      pitches[i]=-10; // off.
+      setStepNote(i,NNOTES,0.5); // no note, inactive
     }
+
     step=0;
     metroPhase=0.f;
+    phase=0.f;
+  }
 
+  void setStepNote(int step,int note,float bright) {
+    // step: 0 to NSTEPS-1
+    // note: -1 to leave as is, 0 to NNOTES-1 to set note, NNOTES for no note
+    if ( note>=0 ) {
+      pitches[step]=noteValues[note];
+      stepColor[step][0]=noteColor[note][0];
+      stepColor[step][1]=noteColor[note][1];
+      stepColor[step][2]=noteColor[note][2];
+    } else {
+      ; // leave note as is
+    }
+
+    if (bright>=0.0) {
+      lights[STEP_LIGHT+step*NCOLORS+0].value = bright*stepColor[step][0];
+      lights[STEP_LIGHT+step*NCOLORS+1].value = bright*stepColor[step][1];
+      lights[STEP_LIGHT+step*NCOLORS+2].value = bright*stepColor[step][2];
+    } else {
+      ; // no GUI update
+    }
   }
 
   void process(const ProcessArgs &args) override {
@@ -83,23 +93,31 @@ struct Duetto : Module {
     float tempo=1.f; // 120 bpm
     float clockTime = std::pow(2.f, tempo); // tempo in Hz
 
+    for (int t=0;t<NNOTES;t++ ) {
+      if (noteTriggers[t].process(params[NOTE_PARAM+t].getValue() > 0.f)) {
+        setStepNote(step,t,1.0); 
+      }
+    }
+
+    for (int s=0;s<NSTEPS;s++ ) {
+      if (stepTriggers[s].process(params[STEP_PARAM+s].getValue() > 0.f)) {
+        setStepNote(s,NNOTES,0.5); // default to inactive
+      }
+    }
+    
     metroPhase += clockTime * args.sampleTime;
     if (metroPhase >= 1.f) {
       // Turn current light off:
-      lights[STEP_LIGHT+step].value = 0;
+      setStepNote(step,-1,0.5);
+      
       step++;
       metroPhase-=1.f;
       if (step>=NSTEPS)
         step=0;
-      lights[STEP_LIGHT+step].value = 1.f;
+
+      setStepNote(step,-1,1.0);
     }
-    
-    for (int t=0;t<NNOTES;t++ ) {
-      if (noteTriggers[t].process(params[NoteParams[t]].getValue() > 0.f)) {
-        pitches[step]=noteValues[t];
-      }
-    }
-    
+        
     // Compute the frequency from the pitch parameter and input
     float pitch = pitches[step];
 
@@ -139,16 +157,16 @@ struct DuettoWidget : ModuleWidget {
     addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-    addParam(createParamCentered<CKD6>(mm2px(Vec(13.215,  99.829)), module, Duetto::NOTE00_PARAM));
-    addParam(createParamCentered<CKD6>(mm2px(Vec(26.830,  99.829)), module, Duetto::NOTE01_PARAM));
-    addParam(createParamCentered<CKD6>(mm2px(Vec(40.444,  99.829)), module, Duetto::NOTE02_PARAM));
-    addParam(createParamCentered<CKD6>(mm2px(Vec(54.059,  99.829)), module, Duetto::NOTE03_PARAM));
-    addParam(createParamCentered<CKD6>(mm2px(Vec(67.674,  99.829)), module, Duetto::NOTE04_PARAM));
-    addParam(createParamCentered<CKD6>(mm2px(Vec(13.215, 112.498)), module, Duetto::NOTE05_PARAM));
-    addParam(createParamCentered<CKD6>(mm2px(Vec(26.830, 112.498)), module, Duetto::NOTE06_PARAM));
-    addParam(createParamCentered<CKD6>(mm2px(Vec(40.444, 112.498)), module, Duetto::NOTE07_PARAM));
-    addParam(createParamCentered<CKD6>(mm2px(Vec(54.059, 112.498)), module, Duetto::NOTE08_PARAM));
-    addParam(createParamCentered<CKD6>(mm2px(Vec(67.674, 112.498)), module, Duetto::NOTE09_PARAM));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(13.215,  99.829)), module, Duetto::NOTE_PARAM+0));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(26.830,  99.829)), module, Duetto::NOTE_PARAM+1));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(40.444,  99.829)), module, Duetto::NOTE_PARAM+2));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(54.059,  99.829)), module, Duetto::NOTE_PARAM+3));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(67.674,  99.829)), module, Duetto::NOTE_PARAM+4));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(13.215, 112.498)), module, Duetto::NOTE_PARAM+5));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(26.830, 112.498)), module, Duetto::NOTE_PARAM+6));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(40.444, 112.498)), module, Duetto::NOTE_PARAM+7));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(54.059, 112.498)), module, Duetto::NOTE_PARAM+8));
+    addParam(createParamCentered<CKD6>(mm2px(Vec(67.674, 112.498)), module, Duetto::NOTE_PARAM+9));
 
     addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(88.257, 58.196)), module, Duetto::OUT1_OUTPUT));
 
@@ -158,12 +176,21 @@ struct DuettoWidget : ModuleWidget {
       float theta=step*2*M_PI/NSTEPS;
       float x=centerX + rad*std::cos(theta);
       float y=centerY + rad*std::sin(theta);
-      addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(x,y)), module, Duetto::STEP_LIGHT+step));
+      
+      // Following Mutes from Fundamental
+      addParam( createParamCentered<LEDBezel>(mm2px(Vec(x,y)), module, Duetto::STEP_PARAM + step));
+      
+      addChild( createLightCentered<LEDBezelLight<RedGreenBlueLight>>(mm2px(Vec(x,y)), module,
+                                                                      Duetto::STEP_LIGHT + step*NCOLORS));
     }
   }
 };
 
 
 Model* modelDuetto = createModel<Duetto, DuettoWidget>("Duetto");
+
+
+// NEXT:
+//  Update lights at init, and when their state changes.
 
 
